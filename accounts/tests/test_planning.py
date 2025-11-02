aea86e9c7ae21004338b014ba5eee8b06aff8897
eloiseschmitt <eschmitt18@gmail.com>
2025-11-02 09:19:55 +0100

    fix pylint

--- accounts/tests/test_planning.py
+++ accounts/tests/test_planning.py
@@ -9,10 +9,12 @@ class PlanningHelpersTests(TestCase):
     """Validate layout computations for planning blocks."""
 
     def test_compute_block_returns_expected_percentages(self):
+        """Test that _compute_block returns correct top and height percentages."""
         block = _compute_block("09:00", "10:30")
         self.assertAlmostEqual(block["top_pct"], (60 / (12 * 60)) * 100)
         self.assertAlmostEqual(block["height_pct"], (90 / (12 * 60)) * 100)
 
     def test_compute_block_minimum_duration(self):
+        """Test that _compute_block enforces a minimum height percentage."""
         block = _compute_block("09:00", "09:05")
         self.assertAlmostEqual(block["height_pct"], (30 / (12 * 60)) * 100)
